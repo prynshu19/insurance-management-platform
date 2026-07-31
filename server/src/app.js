06d1c59ee364d834303e0cb9134e6a13a78133ad
prynshu19 <priyanshupanwar780@gmail.com>
2026-07-31 11:53:17 +0530

    create first API

--- server/src/app.js
+++ server/src/app.js
@@ -4,4 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Insurance Management API Running",
+  });
+});
+
 module.exports = app;
