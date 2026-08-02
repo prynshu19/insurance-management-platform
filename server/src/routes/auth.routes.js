18717793780165bb92c56f2ffd0e96c321d064c7
prynshu19 <priyanshupanwar780@gmail.com>
2026-08-02 17:15:58 +0530

    Successfully Created Role based Authorization middleware

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -9,8 +9,11 @@ const {
   login,
   getProfile,
 } = require("../controllers/auth.controller");
+const { authorize } = require("../middlewares/role.middleware");
 
 router.get("/profile", protect, getProfile);
+
+router.post("/", protect, authorize("ADMIN,AGENT"), createCustomer);
 
 router.post("/register", register);
 
